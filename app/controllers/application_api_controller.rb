c8e82a1f78a70aae5b5235d27aff9fa61f7691a0
Julia Will <julia@hoverstate.com>
2015-12-08 21:55:02 +0100

    Updates Api Controller to protect from forgery

--- app/controllers/application_api_controller.rb
+++ app/controllers/application_api_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationApiController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
+
   respond_to :json
 
 end
