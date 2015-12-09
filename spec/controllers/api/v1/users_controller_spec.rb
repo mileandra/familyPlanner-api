fecf41ed9991d1a09b03cdb9ed18a86eea6b7cdc
Julia Will <julia@hoverstate.com>
2015-12-09 13:57:29 +0100

    refactor: Clean up user specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,8 +10,7 @@ describe Api::V1::UsersController do
     end
 
     it 'return the information about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eql @user.email
+      expect(json_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
@@ -26,8 +25,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json for the user created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql @user_attributes[:email]
+        expect(json_response[:email]).to eql @user_attributes[:email]
       end
 
       it { should respond_with 201 }
@@ -40,13 +38,11 @@ describe Api::V1::UsersController do
       end
 
       it 'renders errors as json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_response).to have_key(:errors)
       end
 
       it 'renders the errors on why it could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include "can't be blank"
+        expect(json_response[:errors][:email]).to include "can't be blank"
       end
 
       it { should respond_with 422 }
@@ -61,8 +57,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the updated user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql 'newmail@example.com'
+        expect(json_response[:email]).to eql 'newmail@example.com'
       end
 
       it { should respond_with 200 }
@@ -75,13 +70,11 @@ describe Api::V1::UsersController do
       end
 
       it 'renders an error json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_response).to have_key(:errors)
       end
 
       it 'renders the errors on why it could not be updated' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include 'is invalid'
+        expect(json_response[:errors][:email]).to include 'is invalid'
       end
 
       it { should respond_with 422 }
