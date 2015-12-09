03bc919696ccd1fa4f283be7091debced97e619c
Julia Will <julia@hoverstate.com>
2015-12-09 08:56:13 +0100

    Adds the user create endpoint

--- app/controllers/application_api_controller.rb
+++ app/controllers/application_api_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationApiController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
+  include ActionController::RequestForgeryProtection
   protect_from_forgery with: :null_session
 
   respond_to :json
