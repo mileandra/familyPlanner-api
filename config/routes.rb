292140f14536085763a72eb5c9a8ad16f7b3c749
Julia Will <julia@hoverstate.com>
2015-12-09 15:01:54 +0100

    Feature: Add user sign out  method

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, :defaults => {format: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
-      resources :sessions, :only => [:create]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 
--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -23,5 +23,28 @@ describe Api::V1::SessionsController do
 
       it { should respond_with 200 }
     end
+
+    context 'when the credentials are incorrect' do
+      before(:each) do
+        credentials = { email: @user.email, password: "invalidPassword" }
+        post :create, { session: credentials }
+      end
+
+      it 'returns a json with an error' do
+        expect(json_response[:errors]).to eql "Invalid email or password"
+      end
+      it { should respond_with 422 }
+    end
+  end
+
+
+  describe 'DELETE #destroy' do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
   end
 end
