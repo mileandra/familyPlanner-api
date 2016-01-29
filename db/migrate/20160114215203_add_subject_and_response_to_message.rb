7d31df7cdb1e3bc8b8de37aaa1adf9031ba8de8f
Julia Will <me@juliawill.com>
2016-01-29 11:22:42 +0100

    Fix messages reference

--- db/migrate/20160114215203_add_subject_and_response_to_message.rb
+++ db/migrate/20160114215203_add_subject_and_response_to_message.rb
@@ -2,6 +2,6 @@ class AddSubjectAndResponseToMessage < ActiveRecord::Migration
   def change
     add_column :messages, :subject, :string
     add_reference :messages, :responds
-    add_foreign_key :messages, :messages
+    add_foreign_key :messages, :messages, column: :responds_id
   end
 end
